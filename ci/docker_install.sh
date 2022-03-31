2363cdd50d224301a7d4ef864987d29c1db81c22
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-03-31 14:24:09 +0200

    [CI] install composer for PHP jobs

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,5 +9,13 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 
+# Install Composer
+wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php'); unlink('installer.sig');"
+php composer.phar install
+    
 # Install our PHP dependencies
 composer install
