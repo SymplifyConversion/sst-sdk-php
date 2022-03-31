d9175446285999a64f1668a0cb8cedc42a57a4a6
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-03-31 15:36:59 +0200

    [CI] composer is not in PATH

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -17,7 +17,6 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php'); unlink('installer.sig');"
-php composer.phar install
     
 # Install our PHP dependencies
-composer install
+php composer.phar install
