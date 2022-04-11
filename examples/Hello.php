050b61ed8e6a11e3c7b8a74aeecced1c184c05c1
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-04-11 09:59:46 +0200

    [examples] get and print config

--- examples/Hello.php
+++ examples/Hello.php
@@ -16,4 +16,16 @@ $cdnBaseURL = getenv('SSTSDK_CDN_BASEURL');
 
 $sdk = new SSTSDK\Client($websiteID, $pool, $cdnBaseURL);
 
+// simple cache exercise
 echo $sdk->hello() . PHP_EOL;
+
+echo "Getting config from:";
+echo $sdk->getConfigURL() . " ... ";
+$cfg = $sdk->fetchConfig();
+echo "OK" . PHP_EOL;
+
+printf("Projects (as of %s)\n", date("c", $cfg->updated));
+
+foreach ($cfg->projects as $proj) {
+    echo " * $proj->id: $proj->name" . PHP_EOL;
+}
