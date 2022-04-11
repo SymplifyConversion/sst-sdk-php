f9e0cbe7f3b43816da3205fd99651a5f344abcbd
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-04-11 17:32:44 +0200

    [lint] fix phpstan CLI syntax

--- ci/lint.sh
+++ ci/lint.sh
@@ -2,12 +2,18 @@
 
 set -xe
 
+
 find src -name '*.php' -print0 | xargs -0 -P4 -n1 php -l
 
 composer phpcs
 
-./vendor/bin/phpstan --memory-limit 256M
+./vendor/bin/phpstan --memory-limit=256M
 
 cd examples
+
+find src -name '*.php' -print0 | xargs -0 -P4 -n1 php -l
+
+# examples needs this because it's not in the default before_script
 composer install
-../vendor/bin/phpstan --memory-limit 256M
+
+../vendor/bin/phpstan --memory-limit=256M
