a8afc21cfa296b2076c3dac2a1715fb8d20e7e44
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-04-11 17:23:23 +0200

    [lint] give phpstan more memory

--- ci/lint.sh
+++ ci/lint.sh
@@ -6,8 +6,8 @@ find src -name '*.php' -print0 | xargs -0 -P4 -n1 php -l
 
 composer phpcs
 
-./vendor/bin/phpstan
+./vendor/bin/phpstan --memory-limit 256M
 
 cd examples
 composer install
-../vendor/bin/phpstan
+../vendor/bin/phpstan --memory-limit 256M
